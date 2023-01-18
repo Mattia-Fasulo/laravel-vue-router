c817ec98b892e6bdff2a94ae1d32b6351d332aae
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-01-18 18:12:11 +0100

    add card component

---
+++ src/components/componentsGeneral/CardComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    
+        <div class="col-12 col-md-4 d-flex justify-content-center mt-5" v-for="(project, index) in obj" :key="index">
+            <div class="card" style="width: 18rem;">
+                <img :src="`${store.imagePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ truncateContent(project.description) }}</p>
+                    <router-link class="btn btn-secondary max-content"
+                        :to="{ name: 'single-project', params: { slug: project.slug } }">
+                        See the project
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    
+</template>
+
+<script>
+import { store } from '../../store';
+
+export default {
+    name: 'CardComponent',
+    props: {
+        obj: Array
+    },
+    data(){
+        return {
+            store,
+            contentMaxLen: 60
+        }
+    },
+    methods: {
+        truncateContent(text) {
+            if (text.length > this.contentMaxLen) {
+                return text.substr(0, this.contentMaxLen) + "...";
+            }
+            return text;
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/ProjectsListPage.vue
+++ src/pages/ProjectsListPage.vue
@@ -2,20 +2,7 @@
     <section>
         <h1 class="text-center my-3">Projects List</h1>
         <div class="row">
-            <div class="col-12 col-md-4 d-flex justify-content-center mt-5" v-for="(project, index) in projects"
-                :key="index">
-                <div class="card" style="width: 18rem;">
-                    <img :src="`${store.imagePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
-                    <div class="card-body d-flex flex-column align-items-center">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <p class="card-text">{{ truncateContent(project.description) }}</p>
-                        <router-link class="btn btn-secondary max-content"
-                            :to="{ name: 'single-project', params: { slug: project.slug } }">
-                            See the project
-                        </router-link>
-                    </div>
-                </div>
-            </div>
+            <CardComponent :obj="this.projects"/>
         </div>
         <nav aria-label="Page-navigation" class="d-flex justify-content-center mt-5">
             <ul class="pagination">
@@ -30,8 +17,14 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import CardComponent from '../components/componentsGeneral/cardComponent.vue';
+
+
 export default {
-    name: 'ProjectsListPage',
+    name: "ProjectsListPage",
+    components: { 
+        CardComponent 
+    },
     data() {
         return {
             store,
@@ -40,28 +33,29 @@ export default {
             currentPage: 1,
             lastPage: null,
             total: 0
-        }
+        };
     },
     methods: {
         getProjects(pagenum) {
-            axios.get(`${this.store.apiURL}/projects`, { params:{page: pagenum}  }).then((response) => {
-                console.log(response.data.result.last_page)
+            axios.get(`${this.store.apiURL}/projects`, { params: { page: pagenum } }).then((response) => {
+                console.log(response.data.result.last_page);
                 this.projects = response.data.result.data;
                 this.currentPage = response.data.result.current_page;
                 this.lastPage = response.data.result.last_page;
                 this.total = response.data.result.total;
-            })
+            });
         },
         truncateContent(text) {
             if (text.length > this.contentMaxLen) {
-                return text.substr(0, this.contentMaxLen) + '...';
+                return text.substr(0, this.contentMaxLen) + "...";
             }
             return text;
         }
     },
     mounted() {
         this.getProjects(1);
-    }
+    },
+    
 }
 </script>
 
